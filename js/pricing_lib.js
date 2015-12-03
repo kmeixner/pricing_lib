713368328db357daf5e0ed2a1d540f4324f31d3a
Kevin Meixner <kevin@slyce.it>
2015-12-03 13:40:05 -0500

    Added JSDoc style source code documentation
    modified:   js/pricing_lib.js

--- js/pricing_lib.js
+++ js/pricing_lib.js
@@ -1,29 +1,95 @@
+/** 
+* @class PricingLib
+* 
+* This class provides methods to calculate pricing rates.
+*/
 function PricingLib() {
 
+	// *** PROPERTIES ***:
+
+	/**
+	 * @private {object} self: convenient reference to class template from 
+	 * within itself.
+	 */ 
 	var self = this;
 	
-	self.jsonRateDictionary = 	{
-		base:{flatrate:0.05},
-		person:{personrate:0.012},
-		sector:
-			{
-				pharmaceuticals:0.075,
-				food:0.13,
-				electronics:0.02,
-				other:0.00
-			}
-	};
+	/**
+	 * @public {string} strErrorMsg: a failure error message (or null string ("") if no error has occurred)
+	 */ 	
+	self.strErrorMsg = '';	
 	
-	self.strErrorMsg = '';
+	/**
+	 * @public 
+	 * @property {object} jsonRateDictionary: the markup rate dictionary:
+	 *
+	 * Dictionary Format:	 
+	 * 	{
+	 * 		base:{flatrate:0.05},
+	 * 		person:{personrate:0.012},
+	 * 		sector:
+	 * 			{
+	 * 				pharmaceuticals:0.075,
+	 * 				food:0.13,
+	 * 				electronics:0.02,
+	 * 				other:0.00
+	 * 			}
+	 * 	}
+	 */	 
+	self.jsonRateDictionary = 	
+		{
+			base:{flatrate:0.05},
+			person:{personrate:0.012},
+			sector:
+				{
+					pharmaceuticals:0.075,
+					food:0.13,
+					electronics:0.02,
+					other:0.00
+				}
+		};
 	
+	// *** METHODS ***:	
+	
+	/**
+	 * Returns the rate dictionary:
+	 *
+	 * Dictionary Format:
+	 *
+	 * 	{
+	 * 		base:{flatrate:0.05},
+	 * 		person:{personrate:0.012},
+	 * 		sector:
+	 * 			{
+	 * 				pharmaceuticals:0.075,
+	 * 				food:0.13,
+	 * 				electronics:0.02,
+	 * 				other:0.00
+	 * 			}
+	 * 	}	 
+	 *
+	 * @returns {object}: the rate dictionary
+	 */	
 	self.getRateDictionary = function () {
 		return self.jsonRateDictionary;
 	}
 	
+	/**
+	 * Returns the error message string if an error has occurred.
+	 * Returns nulstring ("") if no error has occurred.
+	 *
+	 * @returns {string}
+	 */		
 	self.getErrorMessage = function () {
 		return self.strErrorMsg;
 	}
 	
+	/**
+	 * Returns the rate for the given sector;
+	 *
+	 * @param {String} sector: 'pharmaceuticals'|'food'|'electronics'|'other'
+	 *
+	 * @returns {Number}: rate for the given sector (or false for an unsupported sector value)
+	 */		
 	self.getRateForSector = function (sector) {
 
 		switch(sector) {
@@ -38,6 +104,34 @@ function PricingLib() {
 		}
 	}
 	
+	/**
+	 * This calculates the markup rate given the base price, sector and number
+	 * of people assigned to work on the job.
+	 *
+	 * How markup is calculated:
+	 *
+     * 1) The baseMarkupPrice is calucated by applying the base flat markup 
+	 * rate to the basePrice
+	 *
+	 * 2) The sector markup percentage is calculated from the baseMarkupPrice
+	 * and added
+	 *
+	 * 3) The markup percentage for the given number of people assigned to 
+	 * work on the job is calculated from the baseMarkupPrice and added	 
+	 *
+	 * ie: Let nBaseMarkup = basePrice*(1+flatMarkupPercent)
+	 *
+	 * Then finalMarkupPrice := 
+	 *	nBaseMarkup
+	 *	+ nSectorPercentage*nBaseMarkup
+	 *	+ numPeople*nPeoplePercentage*nBaseMarkup
+	 *
+	 * @param {Number} basePrice: base price before adding markup
+	 * @param {String} sector: the sector ('pharmaceuticals'|'food'|'electronics'|'other')
+	 * @param {Number} numPeople: number of people assigned to work on job
+	 *
+	 * @returns {Number}: the markup price (or false upon error)
+	 */		
 	self.calculateRate = function (basePrice, sector, numPeople) {
 
 		if (null === basePrice) {
@@ -73,6 +167,5 @@ function PricingLib() {
 		
 		return nFinalPrice;
 	}	
-	
 	
 }
