891d28fd16a4b533ca09565fd57eddec2e74d1b0
Kevin Meixner <kevin@slyce.it>
2015-12-03 13:06:25 -0500

    Now object-oriented (no globals, or potential name conflicts, etc.)
    modified:   index.html
    modified:   js/pricing_lib.js

--- js/pricing_lib.js
+++ js/pricing_lib.js
@@ -1,5 +1,8 @@
-var jsonRateDictionary = 
-	{
+function PricingLib() {
+
+	var self = this;
+	
+	self.jsonRateDictionary = 	{
 		base:{flatrate:0.05},
 		person:{personrate:0.012},
 		sector:
@@ -11,54 +14,65 @@ var jsonRateDictionary =
 			}
 	};
 	
-var strErrorMsg = '';	
+	self.strErrorMsg = '';
 	
-function calculateRate(basePrice, sector, numPeople) {
-
-	if (null === basePrice) {
-		strErrorMsg = 'calculateRate(): basePrice is required.';
-		return false;
+	self.getRateDictionary = function () {
+		return self.jsonRateDictionary;
 	}
 	
-	if (null === sector) {
-		strErrorMsg = 'calculateRate(): sector is required.';
-		return false;
-	}
-
-	if (null === numPeople || numPeople < 1) {
-		strErrorMsg = 'calculateRate(): numPeople is required and must be 1 or more.';	
-		return false;
+	self.getErrorMessage = function () {
+		return self.strErrorMsg;
 	}
 	
-	var nBaseRate = jsonRateDictionary['base']['flatrate'];
-	var nPerPersonRate = jsonRateDictionary['person']['personrate'];	
-	var nSectorRate = getRateForSector(sector);
-	
-	if (false === nSectorRate) {
-		return false;
+	self.getRateForSector = function (sector) {
+
+		switch(sector) {
+			
+			case 'pharmaceuticals': case 'food': case 'electronics': case 'other':
+				return self.jsonRateDictionary['sector'][sector];
+			break;
+			
+			default:
+				self.strErrorMsg = "getRateForSector(): unknown sector received (and 'other' not specified).";
+				return false;
+		}
 	}
 	
-	var nBaseMarkupPrice = (1+nBaseRate)*basePrice;
-    var nPeopleMarkup = numPeople*nPerPersonRate*nBaseMarkupPrice;
-	var nSectorMarkup = nSectorRate*nBaseMarkupPrice;
-	
-	var nFinalPrice = nBaseMarkupPrice+nPeopleMarkup+nSectorMarkup;
-	
-	nFinalPrice = Math.round(nFinalPrice*100)/100; // round off to at most 2 decimals
-	
-	return nFinalPrice;
-}
+	self.calculateRate = function (basePrice, sector, numPeople) {
 
-function getRateForSector(sector) {
-
-	switch(sector) {
-		
-		case 'pharmaceuticals': case 'food': case 'electronics': case 'other':
-			return jsonRateDictionary['sector'][sector];
-		break;
-		
-		default:
-			strErrorMsg = "getRateForSector(): unknown sector received (and 'other' not specified).";
+		if (null === basePrice) {
+			self.strErrorMsg = 'calculateRate(): basePrice is required.';
 			return false;
-	}
+		}
+		
+		if (null === sector) {
+			self.strErrorMsg = 'calculateRate(): sector is required.';
+			return false;
+		}
+
+		if (null === numPeople || numPeople < 1) {
+			self.strErrorMsg = 'calculateRate(): numPeople is required and must be 1 or more.';	
+			return false;
+		}
+		
+		var nBaseRate = self.jsonRateDictionary['base']['flatrate'];
+		var nPerPersonRate = self.jsonRateDictionary['person']['personrate'];	
+		var nSectorRate = self.getRateForSector(sector);
+		
+		if (false === nSectorRate) {
+			return false;
+		}
+		
+		var nBaseMarkupPrice = (1+nBaseRate)*basePrice;
+		var nPeopleMarkup = numPeople*nPerPersonRate*nBaseMarkupPrice;
+		var nSectorMarkup = nSectorRate*nBaseMarkupPrice;
+		
+		var nFinalPrice = nBaseMarkupPrice+nPeopleMarkup+nSectorMarkup;
+		
+		nFinalPrice = Math.round(nFinalPrice*100)/100; // round off to at most 2 decimals
+		
+		return nFinalPrice;
+	}	
+	
+	
 }
